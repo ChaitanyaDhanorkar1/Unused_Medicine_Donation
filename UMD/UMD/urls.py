62e07b09796bb0ff9c124f2247189d5de392a91d
Likhith <cse200001039@iiti.ac.in>
2022-04-08 14:25:25 +0530

    L

--- UMD/UMD/urls.py
+++ UMD/UMD/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+admin.site.site_header = "Just for fun"
+admin.site.site_title = "It takes more"
+admin.site.index_title = "Finally you realize"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
